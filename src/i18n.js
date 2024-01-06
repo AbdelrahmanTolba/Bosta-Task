f6607db2aa8ed71771a2b296f93150ac86b64516
AbdelrahmanTolba <abdelrahman.ibrahim83@gmail.com>
2024-01-07 01:52:44 +0200

    Fix Localization Bug.

--- src/i18n.js
+++ src/i18n.js
@@ -1,12 +1,22 @@
-import i18n from "i18next";
+import i18next from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
-import Backend from "i18next-http-backend";
+import HttpApi from "i18next-http-backend";
 
-i18n
-  .use(Backend)
+i18next
+  .use(HttpApi)
   .use(LanguageDetector)
   .use(initReactI18next)
-  .init({ debug: true, fallbackLang: "en" });
-
-export default i18n;
+  .init({
+    supportedLngs: ["en", "ar"],
+    fallbackLng: "en",
+    debug: true,
+    detection: {
+      order: ["path", "cookie", "htmlTag"],
+      caches: ["cookie"],
+    },
+    react: {
+      wait: true,
+      useSuspense: false,
+    },
+  });
